144d7aa7a1dddf463716b5803f105646fff5d412
Afsana Meem <afsumeem@gmail.com>
2024-02-23 20:31:50 +0600

    wallpaper updated

--- src/pages/WallPaper/WallPaper.jsx
+++ src/pages/WallPaper/WallPaper.jsx
@@ -11,6 +11,7 @@ import left_light from "../../assets/left_light.png";
 import filter_alt from "../../assets/filter_alt.png";
 import photo_library from "../../assets/photo_library.png";
 import likedgray from "../../assets/likedGray.svg";
+import favoriteIcon from "../../assets/liked.png";
 import wallpaper from "../../assets/wallpaper.png";
 import wallpaper1 from "../../assets/wallpaper1.png";
 import wallpaper2 from "../../assets/wallpaper2.png";
@@ -26,6 +27,7 @@ import limg3 from "../../assets/images/wallpaper/limg3.png";
 import Moon from "../../assets/Moon.png";
 import downArrow from "../../assets/downArrow.png";
 import arrow_down_light from "../../assets/arrow_down_light.png";
+import library_add_check from "../../assets/library_add_check.svg";
 import library_add from "../../assets/library_add.svg";
 import { Link } from "react-router-dom";
 import WallpaperSidebar from "../../components/wallpapers-ui/WallpaperSidebar";
@@ -59,6 +61,25 @@ const WallPaper = () => {
   const [openD, setOpenD] = useState(false);
   const [showShareWallpaper, setShowShareWallpaper] = useState(false);
   const [downloadIcon, setDownloadIcon] = useState(true);
+  const [listCheck, setListCheck] = useState(true);
+  const [toggleLike, setToggleLike] = useState(true);
+  const [toggleInput, setToggleInput] = useState(true);
+
+  //
+  // const [open, setOpen] = useState(false);
+  const [show, setshow] = useState(false);
+  const [bars, setBars] = useState(true);
+  const [logo, setLogo] = useState(false);
+
+  const [searchBtn, setSearchBtn] = useState(true);
+
+  const handleLike = () => {
+    setToggleLike(!toggleLike);
+  };
+
+  const handleListCheck = () => {
+    setListCheck(false);
+  };
 
   const handleOpenShare = () => {
     setShowShareWallpaper(true);
@@ -69,6 +90,24 @@ const WallPaper = () => {
     setShowShareWallpaper(false);
     setDownloadIcon(true);
   };
+
+  // new collection
+  const handleToggleInput = () => {
+    setToggleInput(!toggleInput);
+  };
+
+  const handlenavbar_search_btn = () => {
+    setshow(true);
+    setSearchBtn(false);
+    setBars(false);
+    setLogo(true);
+  };
+  const handleCloseIcon = () => {
+    setshow(false);
+    setSearchBtn(true);
+    setBars(true);
+    setLogo(false);
+  };
   return (
     <div className="wallpaper_bg">
       <HeaderDrawer />
@@ -77,7 +116,7 @@ const WallPaper = () => {
         className="container d-flex align-items-center justify-content-between"
         style={{ marginBottom: "18px" }}
       >
-        <div className="d-flex align-items-center" style={{ gap: "29px" }}>
+        <div className="d-flex align-items-center ">
           <Link className="brand" to="/">
             WPS
           </Link>
@@ -105,20 +144,70 @@ const WallPaper = () => {
             />
           </svg> */}
           <div className="position-relative wallPaper_search_container">
-            <div
+            {/* <div
               style={{ top: "5px", left: "10px" }}
               className="position-absolute"
             >
               {iMagnifierG}
-            </div>
+            </div> */}
+            {searchBtn && (
+              <button
+                style={{ top: "5px", left: "10px" }}
+                className=" text-white navbar-search-btn position-absolute"
+                onClick={handlenavbar_search_btn}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <circle
+                    cx="9.16667"
+                    cy="9.16668"
+                    r="5.83333"
+                    stroke="white"
+                    stroke-width="2"
+                  />
+                  <path
+                    d="M16.6667 16.6667L14.1667 14.1667"
+                    stroke="white"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                  />
+                </svg>
+              </button>
+            )}
+
             <input
-              className="w-100 wallPaper_search"
-              style={{ paddingLeft: "35px" }}
+              className=" wallPaper_search"
+              style={{
+                paddingLeft: "35px",
+                marginLeft: "10px",
+                width: "348px",
+              }}
               type="search"
               placeholder="Find more wallpapers..."
             />
           </div>
+
+          {show && (
+            <>
+              <input
+                style={{ marginLeft: "10px" }}
+                className="w-100 SmallInput"
+                type="search"
+                placeholder="Find your next wallpaper..."
+              />
+              <button className="closeBtn px-3" onClick={handleCloseIcon}>
+                {iClose}
+              </button>
+            </>
+          )}
         </div>
+
+        {/*  */}
         <div className="header_profile_container d-none d-md-inline-flex">
           <img className="logo-img" src={profile} alt="" />
         </div>
@@ -188,7 +277,7 @@ const WallPaper = () => {
             </div>
             <div className="wallpaper fontBak d-flex justify-content-end align-items-start pt-2 pe-2 position-relative">
               <div
-                className="btnPosition d-flex justify-content-end align-items-center gap-3 p-1"
+                className="btnPosition d-flex justify-content-end align-items-center gap-3 px-2 py-1"
                 style={{
                   backgroundColor: "rgba(0, 0, 0, 0.60)",
                   width: "fit-content",
@@ -206,23 +295,57 @@ const WallPaper = () => {
                   className="addLibrary"
                   style={{ borderRadius: "5px", opacity: "100%" }}
                 >
-                  <button>
-                    <span>
-                      <img src={library_add} alt="Add Library" />
-                    </span>
-                  </button>
+                  {listCheck ? (
+                    <button>
+                      <span>
+                        <img src={library_add} alt="Add Library" />
+                      </span>
+                    </button>
+                  ) : (
+                    <button>
+                      <span>
+                        <img src={library_add_check} alt="Add Library" />
+                      </span>
+                    </button>
+                  )}
+
                   <div className="libraryOption">
-                    <div className="search-collections justify-content-between d-flex align-items-center gap-2">
-                      <input
-                        className="flex-grow w-100 px-1"
-                        type="search"
-                        placeholder="Search Collections"
-                      />
-                      <button>{add_box}</button>
-                    </div>
+                    {!toggleInput ? (
+                      <>
+                        {/* add new collection */}
+                        <div className="search-collections justify-content-between d-flex align-items-center gap-2">
+                          <input
+                            className="flex-grow w-100 px-1"
+                            type="search"
+                            placeholder="New Collection"
+                          />
+                          <button
+                            className="text-white"
+                            style={{ fontSize: "12px", fontWeight: "100" }}
+                          >
+                            Create
+                          </button>
+                        </div>
+                      </>
+                    ) : (
+                      /* search collection */
+
+                      <div className="search-collections justify-content-between d-flex align-items-center gap-2">
+                        <input
+                          className="flex-grow w-100 px-1"
+                          type="search"
+                          placeholder="Search Collections"
+                        />
+                        <button onClick={() => handleToggleInput()}>
+                          {add_box}
+                        </button>
+                      </div>
+                    )}
+
                     <div className="d-flex flex-column justify-content-start align-items-start">
                       {items?.map((item, index) => (
                         <button
+                          onClick={() => handleListCheck()}
                           key={index}
                           className="collection-list border-bottom w-100"
                         >
@@ -238,11 +361,27 @@ const WallPaper = () => {
                     </div>
                   </div>
                 </div>
-                <button className="like">
-                  <span>
-                    <img src={likedgray} alt="Liked" />
-                  </span>
-                </button>
+                {toggleLike ? (
+                  <button
+                    className="like"
+                    onClick={handleLike}
+                    style={{ height: "20px", width: "20px" }}
+                  >
+                    <span>
+                      <img src={likedgray} alt="Liked" />
+                    </span>
+                  </button>
+                ) : (
+                  <button
+                    onClick={handleLike}
+                    className="like"
+                    style={{ height: "20px", width: "20px" }}
+                  >
+                    <span>
+                      <img src={favoriteIcon} alt="Liked" />
+                    </span>
+                  </button>
+                )}
               </div>
 
               {/* share modal */}
